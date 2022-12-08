cf12f478bde990ffe646f36591e618b8a2272fed
hebbaraditya <adityanhebbar@gmail.com>
2022-12-08 23:15:46 +0530

    few changes, starting from sratch

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from tkinter import Tk, BOTH, Canvas
 class Window(object):
     def __init__(self, width, height):
         self.__root = Tk()
-        self.__title = root
-        self.__canvas = None
-        self.__running = False
+        self.title = root
+        self.canvas = None
+        self.running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
     
     def redraw(self):
@@ -12,12 +12,30 @@ class Window(object):
         self.__root.update()
 
     def wait_for_close(self):
-        self.__running = True
-        while __running:
+        self.running = True
+        while running:
             self.redraw()
     
     def close(self):
-        self.__running = False
+        self.running = False
+
+    def draw_line(Line, fill_color):
+        Line.draw(canvas, fill_color)
+
+class Point(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def draw(Canvas, fill_color):
+        canvas.create_line(
+            x1, y1, x2, y2, fill_color, width = 2
+        )
 
 def main():
     win = Window(800, 600)
